b4b5e270bf2925bf65174ef830ffb93118b1b7d7
KwonOQ <haze5959@naver.com>
2021-10-31 18:58:23 +0900

    ㄴㄹㄴㄹㄴㄹ

--- flutter/good_place_camp/lib/Model/ServerResult.dart
+++ flutter/good_place_camp/lib/Model/ServerResult.dart
@@ -1,19 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
 class ServerResult<DataType> {
-  final bool result;
-  final String msg;
-  final DataType data;
+  bool result;
+  String msg;
+  DataType data;
 
-  ServerResult(this.result, this.msg, this.data);
+  ServerResult({this.result, this.msg, this.data});
 
-  ServerResult.fromMap(Map<String, dynamic> json)
-      : result = json['result'] ?? false,
-        msg = json['msg'] is String ?  json['msg'] : "서버가 불안정합니다. 잠시 후에 다시 시도해주세요.",
-        data = json['data'] as DataType;
+  factory ServerResult.fromJson(Map<String, dynamic> json) => _$ServerResultFromJson(json);
+  Map<String, dynamic> toJson() => _$ServerResultToJson(this);
+  // ServerResult.fromMap(Map<String, dynamic> json)
+  //     : result = json['result'] ?? false,
+  //       msg = json['msg'] is String ?  json['msg'] : "서버가 불안정합니다. 잠시 후에 다시 시도해주세요.",
+  //       data = json['data'] as DataType;
 
-  static ServerResult<DataType> fromJson<DataType>(jsonStr) {
-    final maps = Map<String, dynamic>.from(jsonStr);
-    final result = ServerResult<DataType>.fromMap(maps);
+  // static ServerResult<DataType> fromJson<DataType>(jsonStr) {
+  //   final maps = Map<String, dynamic>.from(jsonStr);
+  //   final result = ServerResult<DataType>.fromMap(maps);
 
-    return result;
-  }
+  //   return result;
+  // }
 }
--- flutter/good_place_camp/lib/Repository/PostsRepository.dart
+++ flutter/good_place_camp/lib/Repository/PostsRepository.dart
@@ -1,73 +1,76 @@
-import 'package:get/get.dart';
 import 'package:good_place_camp/Constants.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
 
 // Model
 import 'package:good_place_camp/Model/ServerResult.dart';
 import 'package:good_place_camp/Model/Post.dart';
 
-class PostsRepository extends GetConnect {
-  @override
-  PostsRepository() {
-    httpClient.defaultDecoder = ServerResult.fromJson;
-    httpClient.baseUrl = BASE_URL;
-  }
+part 'PostsRepository.g.dart';
 
-  Future<Response<ServerResult<dynamic>>> getFirstPagePostsList() {
-    return get<ServerResult<dynamic>>('/home')
-        .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
-  }
+@RestApi(baseUrl: BASE_URL)
+abstract class PostsRepository {
+  factory PostsRepository(Dio dio, {String baseUrl}) = _PostsRepository;
 
-  Future<Response<ServerResult<dynamic>>> getAllPostsSimpleList(
-      int page, List<PostType> typeList) {
-    var url = '/post/list/$page';
-    Iterable<int>.generate(typeList.length).forEach((index) => {
-          if (index == 0)
-            url += "?type=${toInt(typeList[index])}"
-          else
-            url += "&type=${toInt(typeList[index])}"
-        });
+  @GET("/home")
+  Future<ServerResult<HomeInfo>> getHomeInfo();
 
-    return get<ServerResult<dynamic>>(url)
-        .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
-  }
+  // Future<Response<ServerResult<dynamic>>> getFirstPagePostsList() {
+  //   return get<ServerResult<dynamic>>('/home')
+  //       .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+  // }
+// 
+  // Future<Response<ServerResult<dynamic>>> getAllPostsSimpleList(
+  //     int page, List<PostType> typeList) {
+  //   var url = '/post/list/$page';
+  //   Iterable<int>.generate(typeList.length).forEach((index) => {
+  //         if (index == 0)
+  //           url += "?type=${toInt(typeList[index])}"
+  //         else
+  //           url += "&type=${toInt(typeList[index])}"
+  //       });
 
-  Future<Response<ServerResult<dynamic>>> getPostsWith(int id) =>
-      get<ServerResult<dynamic>>('/post/$id')
-          .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+  //   return get<ServerResult<dynamic>>(url)
+  //       .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+  // }
 
-  Future<Response<ServerResult<dynamic>>> getSecretPostsWith(
-          int id, String token) =>
-      get<ServerResult<dynamic>>('/post/$id?token=$token')
-          .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+  // Future<Response<ServerResult<dynamic>>> getPostsWith(int id) =>
+  //     get<ServerResult<dynamic>>('/post/$id')
+  //         .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
 
-  Future<Response<ServerResult<dynamic>>> postPostsWith(
-          int type, String title, String body, String token) =>
-      post<ServerResult<dynamic>>('/post', {
-        "type": type,
-        "title": title,
-        "body": body,
-        "token": token
-      }).timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+  // Future<Response<ServerResult<dynamic>>> getSecretPostsWith(
+  //         int id, String token) =>
+  //     get<ServerResult<dynamic>>('/post/$id?token=$token')
+  //         .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
 
-  Future<Response<ServerResult<dynamic>>> postCommentWith(
-          int postId, String nick, String comment, String token) =>
-      post<ServerResult<dynamic>>('/comment', {
-        "post_id": postId,
-        "nick": nick,
-        "comment": comment,
-        "token": token
-      }).timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+  // Future<Response<ServerResult<dynamic>>> postPostsWith(
+  //         int type, String title, String body, String token) =>
+  //     post<ServerResult<dynamic>>('/post', {
+  //       "type": type,
+  //       "title": title,
+  //       "body": body,
+  //       "token": token
+  //     }).timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
 
-  Future<Response<ServerResult<dynamic>>> postReportWith(
-          String id, String body) =>
-      post<ServerResult<dynamic>>('/report', {"id": id, "body": body})
-          .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+  // Future<Response<ServerResult<dynamic>>> postCommentWith(
+  //         int postId, String nick, String comment, String token) =>
+  //     post<ServerResult<dynamic>>('/comment', {
+  //       "post_id": postId,
+  //       "nick": nick,
+  //       "comment": comment,
+  //       "token": token
+  //     }).timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
 
-  Future<Response<ServerResult<dynamic>>> deletePosts(String token, int id) =>
-      delete<ServerResult<dynamic>>('/post/$token?id=$id')
-          .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+  // Future<Response<ServerResult<dynamic>>> postReportWith(
+  //         String id, String body) =>
+  //     post<ServerResult<dynamic>>('/report', {"id": id, "body": body})
+  //         .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
 
-  Future<Response<ServerResult<dynamic>>> deleteComment(String token, int id, int postId) =>
-      delete<ServerResult<dynamic>>('/comment/$token?id=$id&post_id=$postId')
-          .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+  // Future<Response<ServerResult<dynamic>>> deletePosts(String token, int id) =>
+  //     delete<ServerResult<dynamic>>('/post/$token?id=$id')
+  //         .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
+
+  // Future<Response<ServerResult<dynamic>>> deleteComment(String token, int id, int postId) =>
+  //     delete<ServerResult<dynamic>>('/comment/$token?id=$id&post_id=$postId')
+  //         .timeout(TIMEOUT_SEC, onTimeout: timeoutResponse);
 }
